1ffc07158e83cb3b00996bb895d8a37611f05269
alpha_haga <t-haga@abs-inc.jp>
2026-02-05 23:23:47 +0900

    URL修正

--- backend/nexus-bff/src/main/kotlin/nexus/bff/controller/group/GroupCompaniesController.kt
+++ backend/nexus-bff/src/main/kotlin/nexus/bff/controller/group/GroupCompaniesController.kt
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController
  */
 @Profile("jdbc")
 @RestController
-@RequestMapping("/api/group/companies")
+@RequestMapping("/api/v1/group/companies")
 class GroupCompaniesController(
     private val companyQueryService: CompanyQueryService
 ) {
